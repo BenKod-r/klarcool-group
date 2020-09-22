e4b0ad37954b2cf44561cedd9c9ab67f57bbd7a0
Benkod-r <benk.devweb@gmail.com>
2020-09-22 17:44:32 +0200

    initialize project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Benharrat Khaled.
+ * Date: 22/09/2020.
+ * Time: 17:30
+ */
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController {
+
+    /**
+     * @Route("", name="home_index")
+     */
+    public function home() :Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
