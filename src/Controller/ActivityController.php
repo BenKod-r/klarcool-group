3e15bc14a8f2bb1c2966d5f54bb47fa3e76c914b
Khaleddwm <khaled-benharrat_student2020@wilder.school>
2020-10-26 09:10:21 +0100

    updated

---
+++ src/Controller/ActivityController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Benharrat Khaled.
+ * Date: 22/09/2020.
+ * Time: 17:30
+ */
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/activites")
+ */
+class ActivityController extends AbstractController {
+
+    /**
+     * @Route("", name="blog")
+     */
+    public function blog() :Response
+    {
+        return $this->render('blog/blog.html.twig');
+    }
+
+    /**
+     * @Route("/blog_details", name="blog_details")
+     */
+    public function blogDetails() :Response
+    {
+        return $this->render('blog/blog_details.html.twig');
+    }
+
+    /**
+     * @Route("/elements", name="elements")
+     */
+    public function elements() :Response
+    {
+        return $this->render('blog/elements.html.twig');
+    }
+
+    /**
+     * @Route("/what", name="what")
+     */
+    public function what() :Response
+    {
+        return $this->render('what_do/what_do.html.twig');
+    }
+}
