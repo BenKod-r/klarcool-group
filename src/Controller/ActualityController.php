8853f132162084f706470a95a12c9720eff1e666
Khaleddwm <khaled-benharrat_student2020@wilder.school>
2020-11-08 00:53:36 +0100

    updated activity and login form

--- src/Controller/ActualityController.php
+++ src/Controller/ActualityController.php
@@ -1,26 +1,111 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Benharrat Khaled.
- * Date: 22/09/2020.
- * Time: 17:30
- */
+
 namespace App\Controller;
 
+use App\Entity\Actuality;
+use App\Form\ActualityType;
+use App\Repository\ActualityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/actualites", name="actualites")
+ * @Route("/actualites")
  */
-class ActualityController extends AbstractController {
+class ActualityController extends AbstractController
+{
+    /**
+     * @Route("", name="actuality_index", methods={"GET"})
+     * @param ActualityRepository $actualityRepository
+     * @return Response
+     */
+    public function index(ActualityRepository $actualityRepository): Response
+    {
+        return $this->render('actuality/index.html.twig', [
+            'actualities' => $actualityRepository->findAll(),
+        ]);
+    }
 
     /**
-     * @Route("", name="")
+     * @Route("/new", name="actuality_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
      */
-    public function actuality() :Response
+    public function new(Request $request): Response
     {
-        return $this->render('actuality/actuality.html.twig');
+        $actuality = new Actuality();
+        $form = $this->createForm(ActualityType::class, $actuality);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($actuality);
+            $entityManager->flush();
+            // do some sort of processing
+            $this->addFlash(
+                'success',
+                'Votre article à bien été enregistré !'
+            );
+
+            return $this->redirectToRoute('actuality_index');
+        }
+
+        return $this->render('actuality/new.html.twig', [
+            'actuality' => $actuality,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="actuality_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Actuality $actuality
+     * @return Response
+     */
+    public function edit(Request $request, Actuality $actuality): Response
+    {
+        $form = $this->createForm(ActualityType::class, $actuality);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            // do some sort of processing
+            $this->addFlash(
+                'success',
+                'Votre article à bien été modifié !'
+            );
+
+            return $this->redirectToRoute('actuality_index');
+        }
+
+        return $this->render('actuality/edit.html.twig', [
+            'actuality' => $actuality,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="actuality_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Actuality $actuality
+     * @return Response
+     */
+    public function delete(Request $request, Actuality $actuality): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$actuality->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($actuality);
+            $entityManager->flush();
+
+            // do some sort of processing
+            $this->addFlash(
+                'danger',
+                'Votre article à bien été supprimé !'
+            );
+        }
+
+        return $this->redirectToRoute('actuality_index');
     }
 }
